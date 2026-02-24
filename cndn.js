6cf7d61a3548bd4da08d25ffc5c84c6f3e7782a7
Medha Sharma <medhasharma302@gmail.com>
2026-02-24 03:30:20 -0300

    add file

---
+++ cndn.js
@@ -0,0 +1,11 @@
+let temperature=25;
+if(temperature>30){
+    console.log("its hot!");
+}
+else if(temperature>20)
+    {
+        console.log("its warm.");
+
+    } else{
+        console.log("its cold");
+    }
---
+++ script.js
@@ -0,0 +1,4 @@
+console.log("hello world");
+var a=10;
+var a=20;
+console.log(a);
